76c111349a9cd255b553960a35f29e3249b2bc41
fenrir <ygyoou88@gmail.com>
2018-08-06 11:59:25 +0800

    first commit

--- src/main/java/com/dreawer/appxauth/AppxAuthApplication.java
+++ src/main/java/com/dreawer/appxauth/AppxAuthApplication.java
@@ -1,5 +1,6 @@
 package com.dreawer.appxauth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 //@SpringBootApplication注解等价于以默认属性使用@Configuration，@EnableAutoConfiguration和@ComponentScan：
 @SpringBootApplication
 @EnableTransactionManagement
-//@MapperScan(basePackages = {"com.dreawer.appxauth.persistence"})
+@MapperScan(basePackages = {"com.dreawer.appxauth.persistence"})
 public class AppxAuthApplication {
 
     public static ApplicationContext applicationContext;
