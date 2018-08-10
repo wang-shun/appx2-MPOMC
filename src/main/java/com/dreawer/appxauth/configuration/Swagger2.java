e869d67a475c27bd2934d7ed506c676b56e19f2a
fenrir <ygyoou88@gmail.com>
2018-08-10 11:24:21 +0800

    add swagger2

--- src/main/java/com/dreawer/appxauth/configuration/Swagger2.java
+++ src/main/java/com/dreawer/appxauth/configuration/Swagger2.java
@@ -26,7 +26,7 @@ public class Swagger2 {
                 .apiInfo(apiInfo())
                 .select()
                 //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.dreawer.*.*.*"))
+                .apis(RequestHandlerSelectors.basePackage("com.dreawer.appxauth.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
